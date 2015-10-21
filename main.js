df35a6c0f556ac4187439788c31a7eb9de6affa1
shui91 <sherman.sy.hui@gmail.com>
2015-10-20 23:05:51 -0700

    feat: Added Item (basically JS Obj) and List class (collection of Items)

--- main.js
+++ main.js
@@ -1,26 +1,53 @@
-(function($){ // demonstrates the declaration and instatiation of a minimalist View
+(function($){ // illustrates how to use a Collection of Models to store data and tie changes to a View
+	var Item = Backbone.Model.extend({
+		defaults: {
+			part1: 'This Is',
+			part2: 'Backbone'
+		}
+	});
+
+	var List = Backbone.Collection.extend({
+		model: Item
+	});
+
 	var ListView = Backbone.View.extend({
-		el: $('body'), // attaches 'this.el' to an existing element (body)
+		el: $('body'),
 
 		events: {
 			'click button#add': 'addItem'
 		},
-		initialize: function(){
-			_.bindAll(this, 'render', 'addItem'); // fixes loss of context for 'this' within methods, every function that uses 'this' should be in here
 
-			this.counter = 0; // total number of items added so far
-			this.render(); // not all views are self-rendering. This one is.
+		initialize: function(){
+			_.bindAll(this, 'render', 'addItem', 'appendItem'); // every fn that uses 'this' as the current obj should be in here!
+
+			this.collection = new List();
+			this.collection.bind('add', this.appendItem); // collection event binder
+
+			this.counter = 0;
+			this.render();
 		},
 
 		render: function(){
+			var self = this;
 			$(this.el).append("<button id='add'>Add List Item</button>");
 			$(this.el).append("<ul></ul>")
-			//$(this.el).append("<ul><li>This is Backbone</li></ul>");
+			_(this.collection.models).each(function(item){ //in case collection is not empty
+				self.appendItem(item);
+			}, this);
 		},
 
 		addItem: function(){
 			this.counter++;
-			$('ul', this.el).append("<li>Counted " + this.counter + " Times</li>");
+			var item = new Item();
+			item.set({
+				part2: item.get('part2') + " " + this.counter // modify item defaults
+			});
+			this.collection.add(item); // add item to collection; view is updated via event 'add'
+			// $('ul', this.el).append("<li>Counted " + this.counter + " Times</li>");
+		},
+
+		appendItem: function(item){
+			$('ul', this.el).append("<li>" + item.get('part1')+" "+item.get('part2') + "</li>");
 		}
 	});
 
